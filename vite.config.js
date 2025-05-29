53b9afc00c6a6101167175fea39f03ed066e158b
jiyad prawira <ristiantoalan1@gmai.com>
2025-05-29 18:55:59 +0700

    chore: local changes before pull

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,38 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg', 'favicon.ico', 'logo192.png', 'logo512.png'],
+      manifest: {
+        name: 'User Management App',
+        short_name: 'Asuransi',
+        description: 'User Management App PWA',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
   ],
   theme: {
     extend: {},
